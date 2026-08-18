b80dbfff4d9f109f1ae4d8c1527d984852cfa1c1
balaji-pbh <alokdurgavivek667@gmail.com>
2026-08-18 10:45:32 +0530

    Add product PRD-U80ZVD

--- data.js
+++ data.js
@@ -16,4 +16,22 @@ var CATEGORIES=[
   }
 ];
 
-var PRODUCTS=[];
+var PRODUCTS=[
+  {
+    "id": "PRD-U80ZVD",
+    "category": "swk",
+    "name": "Stylish short kurti",
+    "price": 249,
+    "oldPrice": 500,
+    "discount": 50,
+    "rating": 4.5,
+    "ratingCount": 0,
+    "images": [
+      "images/products/file-00000000fd7082088cb9691163c8c466-1787030123637-b9u3e.png"
+    ],
+    "description": "Add effortless style to your everyday wardrobe with this elegant Women's Printed Short Kurti. Crafted from soft and breathable fabric, this navy blue short kurti features an attractive floral print, a stylish round neckline with tie-up detailing, and comfortable 3/4 sleeves. Perfect for pairing with jeans, trousers, or leggings, it's an ideal choice for daily wear, office, college, and casual outings.",
+    "details": {
+      "Women's Short kurti": "Premium everyday wear short kurti with soft fabric, stylish floral print, comfortable regular fit, and versatile design for office, college, and casual wear."
+    }
+  }
+];
